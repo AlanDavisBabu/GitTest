1863bffa9c347aa99d2055589f8931d738e36425
lida12 <lidajacob4@gmail.com>
2018-07-17 18:30:13 +0530

    Comment question

--- q1.php
+++ q1.php
@@ -1,4 +1,10 @@
 <?php
+/*1. Write a PHP script to print the following,
+● 'I'm about to learn PHP!'
+● 'You could use Zend\Symfony'
+● "It's called "Product Tree""
+● "\0 is called as a null character"
+Use the inputs as such (including the quotes)*/ 
 echo "'I'm about to learn PHP!'<br>";
 echo "'You could use Zend\Symfony'<br>";
 echo "\"It's called \"Product Tree\"\"<br>";
