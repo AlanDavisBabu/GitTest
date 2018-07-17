363e5cc9a9ac3234a47a6bf8e1d2c8b2bd6a3550
Alan Davis Babu <alanb@qburst.com>
2018-07-17 18:30:25 +0530

    haaiiiioo

--- q1.php
+++ q1.php
@@ -3,3 +3,5 @@ echo "'I'm about to learn PHP!'<br>";
 echo "'You could use Zend\Symfony'<br>";
 echo "\"It's called \"Product Tree\"\"<br>";
 echo "\"\\0 is called as a null character\"";
+echo "\"It's called \"Product Tree\"\"<br>";
+echo "\"\\0 is called as a null character\"";
